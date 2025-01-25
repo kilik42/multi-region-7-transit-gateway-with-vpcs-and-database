b62c0431cff10c9bef3f9b49cd7a0a669f98888d
kilik42 <marvinevins@yahoo.com>
2025-01-25 14:02:13 -0600

    adding comments

---
+++ modules/db/main.tf
@@ -0,0 +1,20 @@
+# resource for aws db instance
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  db_name              = "mydb"
+  engine               = "mysql"
+  engine_version       = "8.0"
+  instance_class       = "db.t3.micro"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql8.0"
+  skip_final_snapshot  = true
+}
+
+
+# resource for aws db subnet group
+
+
+
+# resource for aws db security group
+
---
+++ modules/db/output.tf
@@ -0,0 +1,26 @@
+# output for instance id
+
+# output "db_instance_id" {
+#   value = aws_db_instance.default.id
+# }
+
+
+# output for instance endpoint
+
+
+
+####### might need this stuff
+
+# output "db_subnet_group_id" 
+
+
+# output "db_subnet_group_name" 
+
+
+
+# output "db_subnet_group_vpc_id" 
+
+
+
+# output "db_subnet_group_subnet_ids" 
+
